0c29a560383ee1146f84e8db69c6096bb0290364
hylosy <k.hironori0825@gmail.com>
2026-01-03 17:21:33 +0900

    Feat: Add eventType column.

--- src/main/kotlin/hylosy/pcea/dao/EventDao.kt
+++ src/main/kotlin/hylosy/pcea/dao/EventDao.kt
@@ -9,10 +9,13 @@ class EventDao {
         EventTable.batchUpsert(events) { event ->
             this[EventTable.id] = event.id
             this[EventTable.name] = event.name
+            this[EventTable.eventType] = event.eventType.id
         }
     }
 }
 
+// TODO: Exposed replace to DAO style
+// The compiler doesn't catch it when a developer forgets to set a new property in the insert method.
 object EventTable : Table("events") {
     // NOTE: Set default value because of batch insert.
     val id = integer("id").default(0)
