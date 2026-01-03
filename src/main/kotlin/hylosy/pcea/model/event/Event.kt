24c9cac5b17aa60d672c357770608020599e257a
hylosy <k.hironori0825@gmail.com>
2026-01-03 16:26:28 +0900

    Feat: Define eventType.

--- src/main/kotlin/hylosy/pcea/model/event/Event.kt
+++ src/main/kotlin/hylosy/pcea/model/event/Event.kt
@@ -4,3 +4,16 @@ data class Event(
     val id: Int,
     val name: String,
 )
+
+@JvmInline
+value class EventType(
+    val id: Int,
+)
+
+enum class EventTypeId(
+    val id: Int,
+) {
+    ChampionShip(1),
+    CityLeague(2),
+    SealedBattle(7),
+}
